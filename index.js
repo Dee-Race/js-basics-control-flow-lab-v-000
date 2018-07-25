8e02a8408c796414643dc36437dd7c12562540a2
Dee Race <deerace12@gmail.com>
2018-07-25 19:46:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
-    result = 'Thank you so much.'
-    break;
+      result = 'Thank you so much.'
+      break;
     case 'not as generous':
-    result = 'Thank you.'
-    break;
-    default: 
-    result = 'Bye.'
-    break;
+      result = 'Thank you.'
+      break;
+    default:
+      result = 'Bye.'
+      break;
   }
   return result
 }
