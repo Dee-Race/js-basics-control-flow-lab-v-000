20cf810535530f8eedcf0895bfb351956deb52f4
Dee Race <deerace12@gmail.com>
2018-07-25 19:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function scuberGreetingForFeet(someValue) {
 function ternaryCheckCity(city) {
   return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
+
+function switchOnCharmFromTip(tip) {
+  
+}
