c30621250a9ba81818d883fdc467bc2ed7ca82a4
Dee Race <deerace12@gmail.com>
2018-07-25 19:34:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet (someValue) {
-
+function scuberGreetingForFeet(someValue) {
+  let result 
+  if (someValue <= 400) {
+    result = "This one's on me!"
+  }
+  return result
 }
